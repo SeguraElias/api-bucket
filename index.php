9e0b875f8f7a6c447866115ec6b5c5544d9e7fc7
Elias Segura <eliasjsegura00@gmail.com>
2025-05-11 16:17:25 -0600

    changes

--- index.php
+++ index.php
@@ -2,6 +2,16 @@
 require_once __DIR__ . '/src/config/s3config.php';
 require_once __DIR__ . '/src/controllers/imageController.php';
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Expose-Headers: Content-Type, Content-Length");
+header("Content-Type: application/json");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header('Access-Control-Allow-Methods: GET, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type');
+    exit(0);
+}
+
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
     exit();
