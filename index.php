f2fb3b5cf11a9164af2f756159d1b272aeaacd8c
Elias Segura <eliasjsegura00@gmail.com>
2025-05-11 18:33:19 -0600

    ultimo cambio

--- index.php
+++ index.php
@@ -2,14 +2,12 @@
 require_once __DIR__ . '/src/config/s3config.php';
 require_once __DIR__ . '/src/controllers/imageController.php';
 
-// Establecer encabezados CORS globales
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
-// Manejo de solicitudes OPTIONS (preflight)
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    http_response_code(204); // Sin contenido
+    http_response_code(204);
     exit();
 }
 
@@ -22,10 +20,8 @@ $basePath = '/images';
 $endpoint = str_replace($basePath, '', $requestUri);
 $endpoint = rtrim($endpoint, '/');
 
-// Establecer tipo de contenido JSON para todas las respuestas
 header('Content-Type: application/json');
 
-// Manejo de rutas
 if ($requestMethod == 'POST' && strpos($requestUri, '/upload') !== false) {
     echo json_encode($imageController->uploadImage());
     exit;
